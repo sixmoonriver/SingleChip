8c558c25f4ab333c25c6bda662311ed68d11c2ab
yaofayang <yaofayang@qq.com>
2017-12-18 23:52:29 +0800

    update Chinese code

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 # include<REG52.H>
 # include "MCP2515.h"
 # include "MCP2515.c"
-sbit LED_g = P3^4;  			//露篓脪氓LED脰赂脢戮碌脝
+sbit LED_g = P3^4;  			//定义LED指示灯
 sbit LED_r = P3^5;
-bit flag = 0 ;					//露篓脪氓脦禄卤锚脰戮
-unsigned char TX_DATA[8];		//CAN路垄脣脥禄潞鲁氓脟酶
-unsigned char RX_DATA[8];		//CAN陆脫脢脮禄潞鲁氓脟酶
+bit flag = 0 ;					//定义位标志
+unsigned char TX_DATA[8];		//CAN发送缓冲区
+unsigned char RX_DATA[8];		//CAN接收缓冲区
 /*****************************************************************************
-** 潞炉脢媒脙没鲁脝拢潞delayms(unsigned int num)
-** 鹿娄脛脺脙猫脢枚拢潞脩脫脢卤拢卢卤盲脕驴num脭陆麓贸拢卢脩脫脢卤脭陆鲁陇
+** 函数名称：delayms(unsigned int num)
+** 功能描述：延时，变量num越大，延时越长
 *****************************************************************************/
 void delayms(unsigned int num)
 {
@@ -19,62 +19,62 @@ void delayms(unsigned int num)
 	}
 }
 /*****************************************************************************
-** 潞炉脢媒脙没鲁脝拢潞void CAN_ISR()
-** 鹿娄脛脺脙猫脢枚拢潞CAN脰脨露脧麓娄脌铆潞炉脢媒拢卢脧矛脫娄INT1脰脨露脧
+** 函数名称：void CAN_ISR()
+** 功能描述：CAN中断处理函数，响应INT1中断
 *****************************************************************************/
 void CAN_ISR(void) interrupt 2
 {
 	unsigned char num1,num2,num3;
 	unsigned int num;
 	unsigned int i;
-	LED_g = ~LED_g;    					//脰赂脢戮碌脝脳麓脤卢卤盲禄炉
-	num1 = CAN_Read(CANINTF);			//露脕脰脨露脧卤锚脰戮录脛麓忙脝梅拢卢赂霉戮脻脰脨露脧脌脿脨脥拢卢路脰卤冒麓娄脌铆
-	if(num1 & MERRF)					//卤篓脦脛麓铆脦贸脰脨露脧
+	LED_g = ~LED_g;    					//指示灯状态变化
+	num1 = CAN_Read(CANINTF);			//读中断标志寄存器，根据中断类型，分别处理
+	if(num1 & MERRF)					//报文错误中断
 	{
-		CAN_Write(CANINTF,num1 & ~MERRF); //脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~MERRF); //清中断标志
 	}
 	if(num1 & WAKIF)
 	{
-		CAN_Write(CANINTF,num1 & ~WAKIF); //脟氓脰脨露脧卤锚脰戮
-		CAN_Write(CANCTRL,CAN_Normal_Mode);		//禄陆脨脩潞贸拢卢脭脷陆枚录脿脤媒脛拢脢陆拢卢脨毛脡猫脰脙陆酶脠毛脮媒鲁拢鹿陇脳梅脛拢脢陆
-		do										//脜脨露脧脢脟路帽陆酶脠毛脮媒鲁拢鹿陇脳梅脛拢脢陆
+		CAN_Write(CANINTF,num1 & ~WAKIF); //清中断标志
+		CAN_Write(CANCTRL,CAN_Normal_Mode);		//唤醒后，在仅监听模式，须设置进入正常工作模式
+		do										//判断是否进入正常工作模式
 		{
 			num = CAN_Read(CANSTAT);
 		}
 		while(num != CAN_Normal_Mode);
 	}
-	if(num1 & ERRIF)							//麓铆脦贸脰脨露脧
+	if(num1 & ERRIF)							//错误中断
 	{
-		CAN_Write(CANINTF,num1 & ~ERRIF);		//脟氓脰脨露脧卤锚脰戮
-		Pro_CAN_ERROR(); 						//路脰卤冒麓娄脌铆赂梅赂枚麓铆脦贸
+		CAN_Write(CANINTF,num1 & ~ERRIF);		//清中断标志
+		Pro_CAN_ERROR(); 						//分别处理各个错误
 	}
-	if(num1 & TX2IF)							//路垄脣脥2鲁脡鹿娄脰脨露脧
+	if(num1 & TX2IF)							//发送2成功中断
 	{
-		CAN_Write(CANINTF,num1 & ~TX2IF);		//脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~TX2IF);		//清中断标志
 	}
-	if(num1 & TX1IF)							//路垄脣脥1鲁脡鹿娄脰脨露脧
+	if(num1 & TX1IF)							//发送1成功中断
 	{
-		CAN_Write(CANINTF,num1 & ~TX1IF);		//脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~TX1IF);		//清中断标志
 	}
-	if(num1 & TX0IF)							//路垄脣脥0鲁脡鹿娄脰脨露脧
+	if(num1 & TX0IF)							//发送0成功中断
 	{
-		CAN_Write(CANINTF,num1 & ~TX0IF);		//脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~TX0IF);		//清中断标志
 	}
-	if(num1 & RX1IF)							//陆脫脢脮1鲁脡鹿娄脰脨露脧
+	if(num1 & RX1IF)							//接收1成功中断
 	{
-		CAN_Write(CANINTF,num1 & ~RX1IF);		//脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~RX1IF);		//清中断标志
 	}
-	if(num0 & RX0IF)							//陆脫脢脮0鲁脡鹿娄脰脨露脧
+	if(num0 & RX0IF)							//接收0成功中断
 	{
-		CAN_Write(CANINTF,num1 & ~RX0IF); 	   //脟氓脰脨露脧卤锚脰戮
+		CAN_Write(CANINTF,num1 & ~RX0IF); 	   //清中断标志
 		num2 = CAN_Read(RXB0SIDL);
 		num3 = CAN_Read(RXB0DLC);
-		num = num3 & 0x0f; 						//脟贸脢媒戮脻鲁陇露脠
-		if(num2 & IDE)							//脢脮碌陆脌漏脮鹿脰隆
+		num = num3 & 0x0f; 						//求数据长度
+		if(num2 & IDE)							//收到扩展帧
 		{
-			if(num3 & RTR)						//脭露鲁脤脰隆拢卢脭貌露脕脠隆卤锚脢露路没拢卢掳麓脮脮麓脣卤锚脢露路没路垄脣脥脪陋脟贸碌脛脢媒戮脻
+			if(num3 & RTR)						//远程帧，则读取标识符，按照此标识符发送要求的数据
 			{;}
-			else								//脢媒戮脻脰隆拢卢陆脫脢脮麓娄脌铆脢媒戮脻
+			else								//数据帧，接收处理数据
 			{
 				for(i=0;i<num;i++)
 				{
@@ -84,7 +84,7 @@ void CAN_ISR(void) interrupt 2
 		}
 		else
 		{
-			if(num2 & SRR)						//脭露鲁脤脰隆拢卢脭貌露脕脠隆卤锚脢露路没拢卢掳麓脮脮麓脣卤锚脢露赂庐路垄脣脥脪陋脟贸碌脛脢媒戮脻
+			if(num2 & SRR)						//远程帧，则读取标识符，按照此标识府发送要求的数据
 				{;}
 			else
 				{
@@ -98,33 +98,33 @@ void CAN_ISR(void) interrupt 2
 	flag = 1;
 }
 /*****************************************************************************
-** 潞炉脢媒脙没鲁脝拢潞InitSys()
-** 鹿娄脛脺脙猫脢枚拢潞鲁玫脢录禄炉脧碌脥鲁
+** 函数名称：InitSys()
+** 功能描述：初始化系统
 *****************************************************************************/
 void InitSys(void)
 {
 	IT1 = 0;									
-	EX1 = 1;									//脥芒虏驴脰脨露脧1脭脢脨铆
-	EA = 1;										//驴陋路脜脠芦戮脰脰脨露脧
+	EX1 = 1;									//外部中断1允许
+	EA = 1;										//开放全局中断
 }
 
 /*****************************************************************************
-** 潞炉脢媒脙没鲁脝拢潞main()
-** 鹿娄脛脺脙猫脢枚拢潞脰梅潞炉脢媒
+** 函数名称：main()
+** 功能描述：主函数
 *****************************************************************************/
 void main(void)
 {
-	InitSys();									//脧碌脥鲁鲁玫脢录禄炉拢禄
-	CAN_Init();									//CAN鲁玫脢录禄炉拢禄
-	Set_CAN_Baudrate(100);						//脡猫脰脙CAN虏篓脤脴脗脢100Kbit/s
-	CAN_Write(CANCTRL,CAN_Normal_Mode);			//陆酶脠毛脮媒鲁拢脛拢脢陆
-	delayms(100);								//脨隆脩脫脢卤
-	LED_g = 0;									//碌茫脕脕脰赂脢戮碌脝
+	InitSys();									//系统初始化；
+	CAN_Init();									//CAN初始化；
+	Set_CAN_Baudrate(100);						//设置CAN波特率100Kbit/s
+	CAN_Write(CANCTRL,CAN_Normal_Mode);			//进入正常模式
+	delayms(100);								//小延时
+	LED_g = 0;									//点亮指示灯
 	while(1)
 	{
-		CAN_TX_D_Frame(0,8,&TX_DATA[0]);		//脥篓鹿媒CAN路垄脣脥禄潞鲁氓脟酶0路垄脣脥8赂枚脳脰陆脷碌脛脢媒戮脻脰隆
+		CAN_TX_D_Frame(0,8,&TX_DATA[0]);		//通过CAN发送缓冲区0发送8个字节的数据帧
 		delayms(300);							
-		LED_r = ~LED_r;							//脰赂脢戮碌脝脠隆路麓	
+		LED_r = ~LED_r;							//指示灯取反	
 	}
 
 }
